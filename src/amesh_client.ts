45fdec8eadda48eacb739350e1ac80a1db8f8697
nakata <github@satoyan.net>
2017-08-10 07:59:22 +0900

    I can get latest image without timeline

--- src/amesh_client.ts
+++ src/amesh_client.ts
@@ -2,10 +2,17 @@ import * as Jimp from "jimp";
 import * as request from "request";
 import * as _ from "lodash";
 import ImageSize from "./image_size";
+import * as moment from "moment";
 
 class AmeshClient {
     readonly AMESH_TIMELINES_URL = "http://tokyo-ame.jwa.or.jp/scripts/mesh_index.js";
     readonly AMESH_BASE_URL = "http://tokyo-ame.jwa.or.jp";
+
+    // Amesh updadtes every 5 minutes.
+    static readonly AMESH_UPDATE_INTERVAL_MINUTE = 5;
+    static readonly TIME_LINE_FORMAT = "YYYYMMDDHHmm";
+    static readonly DEFAULT_COUNT_CANDIDATE = 3;
+
     private timeLine: string[] = [];
 
     public async getTimeline(): Promise<string[]> {
@@ -33,6 +40,46 @@ class AmeshClient {
                 }
             });
         });
+    }
+
+    /**
+     *
+     * @param max count to length of result
+     * @example Current datetime is 2017/05/30 13:06, max is 3, then result will be
+     * ["201705301305", "201705301300", "201705301255"]
+     */
+    public static getLatestTimeLineCandidates(max: number = 1): string[] {
+        let result: string[] = [];
+        let currentTime = moment();
+        while (result.length < max) {
+            let i = Math.floor(currentTime.minutes() / this.AMESH_UPDATE_INTERVAL_MINUTE);
+            currentTime.minutes(i * this.AMESH_UPDATE_INTERVAL_MINUTE);
+            result.push(currentTime.format(this.TIME_LINE_FORMAT));
+            currentTime.add(-this.AMESH_UPDATE_INTERVAL_MINUTE, "minutes");
+        }
+        return result;
+    }
+
+    public static async getLatestImage(size: ImageSize): Promise<Jimp> {
+        const timeline = this.getLatestTimeLineCandidates(this.DEFAULT_COUNT_CANDIDATE);
+
+        const c = new AmeshClient();
+        for (let i = 0; i < timeline.length; i++) {
+            try {
+                return await c.getImage(size, timeline[i]);
+            } catch (ex) {
+                console.error(`Image not found ${timeline[i]}`);
+            }
+        }
+
+        throw new Error("Image not found");
+    }
+
+    public static async downloadLatestImage(size: ImageSize, dest: string): Promise<Jimp> {
+        if (_.isEmpty(dest)) { throw new Error("dest is empty"); }
+        const img = await this.getLatestImage(size);
+        img.write(dest);
+        return img;
     }
 
     public async getMapImage(size: ImageSize): Promise<Jimp> {
